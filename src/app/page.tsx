ae1af003daf8461f1fe353224609708b64e3002b
akihiro <gmakihiro@gmail.com>
2024-08-17 08:38:11 +0000

    一時

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,14 @@
 import Image from "next/image";
+import { useState } from "react";
 
 export default function Home() {
   return (
     <div>
-      <h1>test</h1>
+      <h1 className="text-3xl font-bold underline">test</h1>
       <form method="POST">
-        
+        番号入力 <input type="text" name="Postal_district_number" />
+        -<input type="text" name="Town_area_number" />
+        <button type="submit" value="検索" />
       </form>
     </div>
   );
